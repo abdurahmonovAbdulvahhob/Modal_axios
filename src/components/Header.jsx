2c7730348573d0fe9133c2faa6550afe6c9638e4
abdurahmonovAbdulvahhob <abdulvahhobabdurahmonov@gmail.com>
2024-12-02 09:21:02 +0500

    added navbar item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,13 +18,13 @@ const Header = ({ signup, login }) => {
           <div className="flex items-center lg:order-2">
             <button
               onClick={login}
-              className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+              className="hidden sm:block text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
             >
               Log in
             </button>
             <button
               onClick={signup}
-              className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+              className="hidden sm:block text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
             >
               Get started
             </button>
@@ -34,7 +34,11 @@ const Header = ({ signup, login }) => {
               className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="mobile-menu-2"
               aria-expanded="false"
-              onClick={()=> {document.getElementById("mobile-menu-2").classList.toggle("hidden")}}
+              onClick={() => {
+                document
+                  .getElementById("mobile-menu-2")
+                  .classList.toggle("hidden");
+              }}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -116,6 +120,22 @@ const Header = ({ signup, login }) => {
                 >
                   Contact
                 </a>
+              </li>
+              <li className="border-b dark:border-gray-700 mb-2">
+                <button
+                  onClick={login}
+                  className="sm:hidden text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                >
+                  Log in
+                </button>
+              </li>
+              <li className="border-b dark:border-gray-700">
+                <button
+                  onClick={signup}
+                  className=" mb-2 sm:hidden text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                >
+                  Get started
+                </button>
               </li>
             </ul>
           </div>
